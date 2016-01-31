92ff33fe57d89135c3983fa13bb113dccb0259c2
Steven Hauser <steven.hauser@zapier.com>
2016-01-31 18:50:57 -0500

    Write the package

---
+++ src/bemify-test.js
@@ -0,0 +1,147 @@
+import test from 'tape';
+import bemify from './bemify';
+
+
+// Object classes
+
+test('object classes - applies classes based on truthiness of value', (t) => {
+  const actual = bemify('block', {
+    '--modifier-a': true,
+    '--modifier-b': false,
+    '--modifier-c': true,
+  });
+  const expected = 'block block--modifier-a block--modifier-c';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('object classes - applies classes to elements based on truthiness of value', (t) => {
+  const actual = bemify('block', {
+    '__element': true,
+    '--modifier-a': true,
+    '--modifier-b': false,
+    '--modifier-c': true,
+  });
+  const expected = 'block__element block__element--modifier-a block__element--modifier-c';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('object classes - does not modify non-bem classes', (t) => {
+  const actual = bemify('block', {
+    '--modifier': true,
+    'some-utility': true,
+    'some-other-utility': false,
+  });
+  const expected = 'block block--modifier some-utility';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('object classes - throws if more than one element is supplied', (t) => {
+  const tryToBemify = () => bemify('block', {
+    '__element-a': true,
+    '__element-b': true,
+  });
+  t.throws(tryToBemify);
+  t.end();
+});
+
+
+// Array classes
+
+test('array classes - applies all truthy classes', (t) => {
+  const actual = bemify('block', [
+    '--modifier-a',
+    null,
+    '--modifier-b',
+  ]);
+  const expected = 'block block--modifier-a block--modifier-b';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('array classes - applies all truthy classes to elements', (t) => {
+  const actual = bemify('block', [
+    '--modifier-a',
+    '__element',
+    null,
+    '--modifier-b',
+  ]);
+  const expected = 'block__element block__element--modifier-a block__element--modifier-b';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('array classes - does not modify non-bem classes', (t) => {
+  const actual = bemify('block', [
+    '--modifier',
+    undefined,
+    'some-utility',
+  ]);
+  const expected = 'block block--modifier some-utility';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('array classes - throws if more than one element is supplied', (t) => {
+  const tryToBemify = () => bemify('block', [
+    '__element-a',
+    false,
+    '__element-b',
+  ]);
+  t.throws(tryToBemify);
+  t.end();
+});
+
+
+// String classes
+
+test('string classes - applies all classes', (t) => {
+  const actual = bemify('block', '--modifier-a --modifier-b');
+  const expected = 'block block--modifier-a block--modifier-b';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('string classes - applies all classes to elements', (t) => {
+  const actual = bemify('block', '__element --modifier-a --modifier-b');
+  const expected = 'block__element block__element--modifier-a block__element--modifier-b';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('string classes - does not modify non-bem classes', (t) => {
+  const actual = bemify('block', '--modifier some-utility');
+  const expected = 'block block--modifier some-utility';
+  t.isEqual(actual, expected);
+  t.end();
+});
+
+test('string classes - throws if more than one element is supplied', (t) => {
+  const tryToBemify = () => bemify('block', '__element-a __element-b');
+  t.throws(tryToBemify);
+  t.end();
+});
+
+
+// No classes
+
+test('no classes - returns the block itself', (t) => {
+  const block = 'block';
+  t.isEqual(bemify(block, null), block);
+  t.isEqual(bemify(block, {}), block);
+  t.isEqual(bemify(block, []), block);
+  t.isEqual(bemify(block, ''), block);
+  t.end();
+});
+
+test('no classes - returns the block itself (curried)', (t) => {
+  const block = 'block';
+  t.isEqual(bemify(block)(), block);
+  t.isEqual(bemify(block)(null), block);
+  t.isEqual(bemify(block)({}), block);
+  t.isEqual(bemify(block)([]), block);
+  t.isEqual(bemify(block)(''), block);
+  t.end();
+});
---
+++ src/bemify.js
@@ -0,0 +1,66 @@
+// Really simple curry function that only supports up to two arguments.
+// This keeps things very simple and very small.
+const curry1 = (fn) => (...args) => (
+  (args.length >= 2) ?
+  fn(...args) :
+  (...moreArgs) => fn(...args, ...moreArgs)
+);
+
+const negate = (fn) => (...args) => !fn(...args);
+
+const isString = (value) => typeof value === 'string';
+const isArray = (value) => Array.isArray(value);
+const isObject = (value) => !!value && (typeof value === 'object') && !isArray(value);
+
+const doesMatch = curry1((regex, string) => regex.test(string));
+
+const isSuffix = doesMatch(/^(--|__)/);
+const isElement = doesMatch(/^__/);
+const isModifier = doesMatch(/^--/);
+
+// Returns sorted array of all keys in `obj` whose values are truthy
+const truthyObjToArr = (obj) => (
+  Object.keys(obj)
+    .filter((key) => !!obj[key])
+    .sort() // facilitate testing
+);
+
+const buildError = (message) => new Error(`bemify - ${message}`);
+
+// Takes `classes` which may be one of several types and returns
+// an array of them with all falsy values filtered out.
+const normalizeSuffixes = (classes) => {
+  let suffixes = [];
+  if (isString(classes)) { suffixes = classes.split(/\s+/); }
+  if (isArray(classes)) { suffixes = classes; }
+  if (isObject(classes)) { suffixes = truthyObjToArr(classes); }
+  return suffixes.filter(Boolean);
+};
+
+// Finds the *only* element in `suffixes` and prefixes it
+// with `block`. If no elements are found in `suffixes`,
+// `block` itself is used. The reason we only allow one
+// element is because a BEM node should never be two BEM
+// elements at once.
+const getElement = (block, suffixes) => {
+  const els = suffixes.filter(isElement);
+  if (els.length > 1) {
+    throw buildError(`Only a single element is allowed, but got ${els.length} (${els.join(', ')}).`);
+  }
+  return `${block}${els[0] || ''}`;
+};
+
+const bemify = curry1((block, denormalizedSuffixes) => {
+  const suffixes = normalizeSuffixes(denormalizedSuffixes);
+  const element = getElement(block, suffixes);
+  const prefix = (suffix) => `${element}${suffix}`;
+  return [
+    element,
+    ...suffixes.filter(isModifier).map(prefix),
+    // Not all items in `suffixes` may be actual BEM suffixes;
+    // some may be standalone classes.
+    ...suffixes.filter(negate(isSuffix)),
+  ].join(' ');
+});
+
+export default bemify;
